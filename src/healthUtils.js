a6bd863120b1e5f00c8fc6d66394f50bb89ed563
MikalJohnson <Johnsonmikal24@gmail.com>
2023-11-13 17:56:13 -0800

    Added previous File Structure

---
+++ src/healthUtils.js
@@ -0,0 +1,11 @@
+function calculateStats(data) {
+    // Implement the logic to calculate stats
+    // ...
+}
+
+function checkHealth(data) {
+    // Implement the logic to check health
+    // ...
+}
+
+module.exports = { calculateStats, checkHealth };
